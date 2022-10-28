43755bdfdf2048ad0b52e54d3d8cc642e845138d
Phillip <71596849+Philliip@users.noreply.github.com>
2022-10-28 15:03:58 +0200

    login login

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap, Bootstrap4
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Length
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = '123456789'
 db = SQLAlchemy(app)
+Bootstrap4(app)
+
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=7, max=80)])
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.is_submitted():
+        print("kokot")
+
+
+    return render_template('login.html', form=form)
+
+
 
 
 if __name__ == '__main__':
