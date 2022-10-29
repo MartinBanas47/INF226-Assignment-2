46a85f329c2175cb1af03d9d90438469aa37bf9a
Martin Baňas <xbanas1@fi.muni.cz>
2022-10-29 23:51:43 +0200

    Added message sending and showing

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
+import datetime
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap4
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import InputRequired, Length
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, login_user, login_required, logout_user
-from models import User
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+from Dto.MessageDto import MessageDto
+from models import User, Message
 from models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = '123456789'
+app.app_context().push()
 db.init_app(app)
+db.create_all()
 Bootstrap4(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=7, max=80)])
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -49,6 +58,7 @@ class RegisterForm(FlaskForm):
     username: StringField = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=7, max=80)])
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -64,11 +74,38 @@ def register():
 
     return render_template('register.html', form=form)
 
+class CreateMessageForm(FlaskForm):
+    receiver: StringField = StringField('Receiver', validators=[InputRequired(), Length(max=20)])
+    message: TextAreaField = TextAreaField('Message', validators=[InputRequired(), Length(max=200)])
+@app.route('/new', methods=['GET', 'POST'])
+def createMessage():
+    form = CreateMessageForm()
+    if form.is_submitted():
+        receiver = User.query.filter_by(username=form.receiver.data).first()
+        new_message = Message(receiver=receiver.id,
+                              message=form.message.data,
+                              sender=current_user.id,
+                              date=datetime.date.today())
+        db.session.add(new_message)
+        db.session.commit()
+    return render_template('createMessage.html', form=form)
+
 @app.route('/messages')
 @login_required
 def messages():
+    #messages_db = db.query(User.username, User.id, Message).join(Message, User.id == Message.sender).filter(Message.receiver== current_user.id).all()
+    messages_db = Message.query.filter_by(receiver=current_user.id).all()
+    messages_list = []
+    for message in messages_db:
+        messages_list.append(MessageDto(
+            message_id=message.id,
+            sender_id=message.sender,
+            sender_username=User.query.filter_by(id=message.sender).first().username    ,
+            timestamp=message.date,
+            message=message.message
+        ))
+    return render_template('messages.html', messages=messages_list, len=len(messages_list))
 
-    return render_template('messages.html')
 
 @app.route('/logout')
 @login_required
--- models.py
+++ models.py
@@ -1,12 +1,12 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
-
-
 '''Table for users'''
 class User(db.Model, UserMixin):
+    __tablename__ = "User"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
@@ -14,5 +14,9 @@ class User(db.Model, UserMixin):
 
 '''Table for messages'''
 class Message(db.Model):
+    __tablename__ = "Message"
     id = db.Column(db.Integer, primary_key=True)
-    
+    message = db.Column(db.String(200), nullable=True)
+    sender = db.Column(db.Integer, ForeignKey("User.id"))
+    receiver = db.Column(db.Integer, ForeignKey("User.id"))
+    date = db.Column(db.Date)
